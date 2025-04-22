38585e89d33bab3f8e0c7be9b4a1b79a64830b05
Jose Luis Hernandez <wichitoooo.20@gmail.com>
2025-04-22 15:11:12 -0600

    barra

--- assets/js/main.js
+++ assets/js/main.js
@@ -212,3 +212,7 @@
     document.addEventListener('scroll', navmenuScrollspy);
   
   })();
+
+  function redirectToPremium() {
+    window.location.href = 'https://wa.me/522211059336?text=Hola!%20Quiero%20acceso%20a%20SystemWoods%202.0!';
+}
